3c7c8b4a9b413c2d3af583036173a48685dead5d
songzhao <13725570410@163.com>
2021-12-20 07:27:35 +0800

    111

--- components/confighead/index.js
+++ components/confighead/index.js
@@ -1,9 +1,10 @@
 
 export default function ConfigHead(props) {
-  const { meta }  = props
-  console.log(meta)
-  return (
-    <div>he</div>
-  )
+  const { meta_data, feature }  = props
+  let fields = meta_data[feature]['fields'] 
+  let meta_keys = Object.keys(fields)
+  let s = meta_keys.map( key =>  <span key = {key} >{fields[key]['name']}</span>)
+  return (<div>
+    {s}
+  </div>)
 }
-
--- pages/config.js
+++ pages/config.js
@@ -4,10 +4,10 @@ import ConfigBody from '../components/configbody'
 const ConfigContext = React.createContext()
 
 export default function Config(props) {
-  const { meta_data, json_data } = props
+  const { meta_data, json_data, feature } = props
   return (
     <ConfigContext.Provider value = {json_data, meta_data}>
-      <ConfigHead meta = {meta_data} />
+      <ConfigHead meta_data = {meta_data} feature = {feature} />
       <ConfigBody />
     </ConfigContext.Provider>
     
@@ -20,8 +20,10 @@ export async function getServerSideProps(context){
   let query_data = await res.json()
   return {
     props: {
+      feature   : query.feature,
+      server_id : query.sid, 
       meta_data : query_data.meta_data,
-      json_data   : query_data.json_data
+      json_data : query_data.json_data
     }
   }
 } 
